3128c8208d74fc64d261e11d01fa76ce984eae79
Gresaaaa <ferataj.gresa@gmail.com>
2025-10-17 18:35:40 +0200

    addProduct created

---
+++ apps/addProduct.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { SafeAreaView, View, StyleSheet } from "react-native";
+import Header from "../components/Header";
+import ProductForm from "../components/ProductForm";
+import ProductForm from "../components/Footer";
