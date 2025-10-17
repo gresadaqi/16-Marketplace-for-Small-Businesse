06a2f010932d0625497fc4d530cbbfb423f9246a
Gresaaaa <ferataj.gresa@gmail.com>
2025-10-17 18:48:22 +0200

    updated

--- apps/addProduct.jsx
+++ apps/addProduct.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { SafeAreaView, View, StyleSheet } from "react-native";
 import Header from "../components/Header";
 import ProductForm from "../components/ProductForm";
-import ProductForm from "../components/Footer";
+import  Footer from "../components/Footer";
 
 export default function App() {
   return (
