fb9333ae5b0fd47a6e03023f94c68a4f1cf3322b
gresadaqi <gresa.daqi11@gmail.com>
2025-10-17 23:24:38 +0200

    Remove files

---
+++ apps/Screens/addProduct.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { SafeAreaView, View, StyleSheet } from "react-native";
+import Header from "../components/Header";
+import ProductForm from "../components/ProductForm";
+import  Footer from "../components/Footer";
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Header />
+      <View style={styles.formWrapper}>
+        <ProductForm />
+      </View>
+    </SafeAreaView>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#d7ceb2ff" ,
+  },
+  });
