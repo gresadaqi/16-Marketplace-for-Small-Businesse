3070603ba4501d9d830b4fdf219f991d786178e6
Gresaaaa <ferataj.gresa@gmail.com>
2025-10-18 09:31:48 +0200

    main category created

--- apps/components/ProductForm.jsx
+++ apps/components/ProductForm.jsx
@@ -8,3 +8,31 @@ import {
   FlatList,
   Image,
 } from "react-native";
+import productImage2 from "../assets/Picture2.png"; // foto e paracaktuar
+
+export default function ProductForm() {
+  const [productName, setProductName] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+  const [selectedImage, setSelectedImage] = useState(productImage2);
+
+
+  const [selected, setSelected] = useState("All");
+  const [open, setOpen] = useState(false);
+  const categories = ["All", "Accessories", "Clothes", "Art", "Other"];
+
+  const handlePickImage = () => {
+    
+    setSelectedImage(productImage2);
+  };
+   const handlePost = () => {
+    console.log({
+      productName,
+      category: selected,
+      price,
+      description,
+      image: selectedImage,
+    });
+  };
+  
+}
