b0f01e822fd7ec8eb6979e9120c3157631194037
Gresaaaa <ferataj.gresa@gmail.com>
2025-10-18 09:30:01 +0200

    new component - ProductForrm

--- apps/components/HeaderAdd.jsx
+++ apps/components/HeaderAdd.jsx
@@ -17,3 +17,33 @@ export default function HeaderAdd() {
     </View>
   );
 }
+const styles = StyleSheet.create({
+  header: {
+    height: 80,
+    backgroundColor: GREEN,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    
+  },
+  title: {
+    color: "#e0d6c8ff",
+    fontSize: 27,
+    fontWeight: "600",
+    textAlign: "center",
+   
+
+   
+  },
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: "white",
+  },
+});
+
---
+++ apps/components/ProductForm.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+  Image,
+} from "react-native";
