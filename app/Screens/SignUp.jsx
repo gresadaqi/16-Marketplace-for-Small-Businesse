582ffbf0f27b5e3f000bd098f6fd4bfa1c8b077e
Festim Gashi <festimi2005gashi@gmail.com>
2025-10-19 17:49:22 +0200

    valid signup fixed

--- app/Screens/SignUp.jsx
+++ app/Screens/SignUp.jsx
@@ -67,8 +67,8 @@ export default function SignUp() {
     }
     if (!ok) return;
 
-    Alert.alert("OK", `SignUp successfully.\nRole: ${role}`, [
-      { text: "OK", onPress: () => router.push("/next") },
+  Alert.alert("OK", `SignUp successfully.\nRole: ${role}`, [
+      { text: "OK", onPress: () => navigation.navigate('Profile') },
     ]);
   };
 
